ed35449b34f824e93c0462f5fe98592f88ca8de4
Ricardo SS <ricardo.rss.dev@gmail.com>
2020-03-06 19:00:37 +0100

    Integration java tools
    * Initial structure for gradle plugin

---
+++ oaboard-gradle-plugin/src/main/java/org/rss/oaboard/tools/gradle/ClientPlugin.java
@@ -0,0 +1,14 @@
+package org.rss.oaboard.tools.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+// TODO: continue https://guides.gradle.org/implementing-gradle-plugins/
+
+public class ClientPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(final Project project) {
+
+    }
+}
